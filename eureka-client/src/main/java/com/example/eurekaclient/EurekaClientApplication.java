24f02a57c7adf6f4ba88f89d636c933977399022
wei.song <wei.song@anker-in.com>
2023-04-11 11:30:03 +0800

    feat: add eureka-client/eureka-server standalone and replica

---
+++ eureka-client/src/main/java/com/example/eurekaclient/EurekaClientApplication.java
@@ -0,0 +1,19 @@
+package com.example.eurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author wei.song
+ * @date 2023/04/11 10:47
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+}
