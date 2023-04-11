3ade1675234c78122d079aba429eec75e11b188e
wei.song <wei.song@anker-in.com>
2023-04-11 17:09:29 +0800

    feat: add user-service provide ribbon call

---
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -0,0 +1,19 @@
+package com.example.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author wei.song
+ * @date 2023/04/11 17:07
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}
