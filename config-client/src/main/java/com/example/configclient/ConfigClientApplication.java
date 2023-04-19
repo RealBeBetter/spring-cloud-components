7786aec0f8474f109764c79e5037738b17e07ab7
wei.song <wei.song@anker-in.com>
2023-04-19 18:59:04 +0800

    feat: add config-server+config-client

---
+++ config-client/src/main/java/com/example/configclient/ConfigClientApplication.java
@@ -0,0 +1,19 @@
+package com.example.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author wei.song
+ * @date 2023/04/19 18:50
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+}
---
+++ config-server/src/main/java/com/example/configserver/ConfigServerApplication.java
@@ -0,0 +1,21 @@
+package com.example.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author wei.song
+ * @date 2023/04/19 18:32
+ */
+@EnableConfigServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
