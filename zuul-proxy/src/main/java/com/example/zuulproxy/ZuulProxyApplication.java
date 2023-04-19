445c14763c1e190910de906cf7269bb984ebc5d0
wei.song <wei.song@anker-in.com>
2023-04-19 15:00:45 +0800

    feat: add Zuul-Proxy module

---
+++ zuul-proxy/src/main/java/com/example/zuulproxy/ZuulProxyApplication.java
@@ -0,0 +1,21 @@
+package com.example.zuulproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author wei.song
+ * @date 2023/04/19 12:05
+ */
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ZuulProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulProxyApplication.class, args);
+    }
+
+}
