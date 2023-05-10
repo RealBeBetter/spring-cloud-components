761d58a4f95bee3f687e2f85508176d4d0980922
Real <swrely@qq.com>
2023-05-10 23:46:20 +0800

    feat: init nacos-service

---
+++ nacos-ribbon-service/src/main/java/com/example/nacosribbonservice/NacosRibbonServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.nacosribbonservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosRibbonServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosRibbonServiceApplication.class, args);
+    }
+
+}
